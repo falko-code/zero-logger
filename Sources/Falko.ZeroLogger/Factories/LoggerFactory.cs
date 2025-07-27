f0e9f9fc879aea5cc335bdfc084044f7a92838f8
Rima Falko <rima@falko.email>
2025-07-27 14:51:48 +0200

    remove unused methods from the logger-factory and optimize it

--- Sources/Falko.ZeroLogger/Factories/LoggerFactory.cs
+++ Sources/Falko.ZeroLogger/Factories/LoggerFactory.cs
@@ -4,18 +4,19 @@ using System.Runtime.CompilerServices;
 
 namespace System.Logging.Factories;
 
+[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
 public readonly struct LoggerFactory(LoggerRuntime loggerRuntime)
 {
-    public static readonly LoggerFactory Global = LoggerRuntime.Global.LoggerFactory;
-
-    public Logger CreateLoggerOfName(string name)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Logger CreateLogger(string name)
     {
         ArgumentNullException.ThrowIfNull(name);
 
         return new Logger(loggerRuntime, name);
     }
 
-    public Logger CreateLoggerOfType<T>() where T : notnull
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Logger CreateLogger<T>() where T : notnull
     {
         var name = typeof(T).FullName;
 
@@ -24,7 +25,8 @@ public readonly struct LoggerFactory(LoggerRuntime loggerRuntime)
         return new Logger(loggerRuntime, name);
     }
 
-    public Logger CreateLoggerOfType(Type type)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Logger CreateLogger(Type type)
     {
         ArgumentNullException.ThrowIfNull(type);
 
@@ -33,40 +35,5 @@ public readonly struct LoggerFactory(LoggerRuntime loggerRuntime)
         ArgumentNullException.ThrowIfNull(name);
 
         return new Logger(loggerRuntime, name);
-    }
-
-    public Logger CreateLoggerOfObject()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Logger CreateLoggerOfMethod<T>([CallerMemberName] string member = null!) where T : notnull
-    {
-        ArgumentNullException.ThrowIfNull(member);
-
-        var name = typeof(T).FullName;
-
-        ArgumentNullException.ThrowIfNull(name);
-
-        return new Logger(loggerRuntime, $"{name}.{member}");
-    }
-
-    public Logger CreateLoggerOfMethod(Type type, [CallerMemberName] string member = null!)
-    {
-        ArgumentNullException.ThrowIfNull(type);
-        ArgumentNullException.ThrowIfNull(member);
-
-        var name = type.FullName;
-
-        ArgumentNullException.ThrowIfNull(name);
-
-        return new Logger(loggerRuntime, $"{name}.{member}");
-    }
-
-    public static Logger CreateLoggerOfMethod([CallerMemberName] string member = null!)
-    {
-        ArgumentNullException.ThrowIfNull(member);
-
-        throw new NotImplementedException();
     }
 }
