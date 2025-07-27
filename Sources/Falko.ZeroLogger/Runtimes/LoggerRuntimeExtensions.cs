e02576ab8b949d15d1d5f0d02dba10b99ea2e6eb
Rima Falko <rima@falko.email>
2025-07-27 15:15:23 +0200

    add modern logger-runtime extensions

---
+++ Sources/Falko.ZeroLogger/Runtimes/LoggerRuntimeExtensions.cs
@@ -0,0 +1,60 @@
+using System.Logging.Builders;
+using System.Runtime.CompilerServices;
+
+namespace System.Logging.Runtimes;
+
+public static class LoggerRuntimeExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static LoggerRuntime Initialize
+    (
+        this LoggerRuntime loggerRuntime,
+        Func<LoggerContextBuilder, LoggerContextBuilder> loggerBuilderFactory
+    )
+    {
+        ArgumentNullException.ThrowIfNull(loggerRuntime);
+        ArgumentNullException.ThrowIfNull(loggerBuilderFactory);
+
+        var loggerBuilder = loggerBuilderFactory(new LoggerContextBuilder());
+
+        loggerRuntime.Initialize(loggerBuilder, CancellationToken.None);
+
+        return loggerRuntime;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static LoggerRuntime Initialize
+    (
+        this LoggerRuntime loggerRuntime,
+        TimeSpan timeout,
+        Func<LoggerContextBuilder, LoggerContextBuilder> loggerBuilderFactory
+    )
+    {
+        ArgumentNullException.ThrowIfNull(loggerRuntime);
+        ArgumentNullException.ThrowIfNull(loggerBuilderFactory);
+
+        var loggerBuilder = loggerBuilderFactory(new LoggerContextBuilder());
+
+        loggerRuntime.Initialize(loggerBuilder, timeout);
+
+        return loggerRuntime;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static LoggerRuntime Initialize
+    (
+        this LoggerRuntime loggerRuntime,
+        CancellationToken cancellationToken,
+        Func<LoggerContextBuilder, LoggerContextBuilder> loggerBuilderFactory
+    )
+    {
+        ArgumentNullException.ThrowIfNull(loggerRuntime);
+        ArgumentNullException.ThrowIfNull(loggerBuilderFactory);
+
+        var loggerBuilder = loggerBuilderFactory(new LoggerContextBuilder());
+
+        loggerRuntime.Initialize(loggerBuilder, cancellationToken);
+
+        return loggerRuntime;
+    }
+}
