b94810a4aa58dbb1cfef510e94d6ed2d4d4859f7
Rima Falko <rima@falko.email>
2025-08-19 10:10:17 +0200

    do to-log-level extension as publish

--- Sources/Falko.ZeroLogger.Microsoft.Logging/Logs/MicrosoftLogLevelExtensions.cs
+++ Sources/Falko.ZeroLogger.Microsoft.Logging/Logs/MicrosoftLogLevelExtensions.cs
@@ -1,6 +1,6 @@
 namespace System.Logging.Logs;
 
-internal static partial class MicrosoftLogLevelExtensions
+public static partial class MicrosoftLogLevelExtensions
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static LogLevel ToLogLevel(this MicrosoftLogLevel microsoftLogLevel)
