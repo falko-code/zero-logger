a6302510df214eb75ed0b6f836ad21da21e3a548
Rima Falko <rima@falko.email>
2025-09-17 04:45:33 +0200

    add lock logger-target for thread-safe logging with single-thread logger targets

---
+++ Sources/Falko.ZeroLogger/Targets/LockLoggerTarget.cs
@@ -0,0 +1,39 @@
+using System.Logging.Contexts;
+using System.Logging.Renderers;
+
+namespace System.Logging.Targets;
+
+public sealed class LockLoggerTarget(LoggerTarget singleThreadLoggerTarget) : LoggerTarget
+{
+#if NET9_0_OR_GREATER
+    private readonly Lock _locker = new();
+#else
+    private readonly object _locker = new();
+#endif
+
+    public override bool OnlySingleThreadAccess => false;
+
+    public override void Initialize(CancellationToken cancellationToken)
+    {
+        lock (_locker)
+        {
+            singleThreadLoggerTarget.Initialize(cancellationToken);
+        }
+    }
+
+    public override void Publish(in LogContext context, ILogContextRenderer renderer, CancellationToken cancellationToken)
+    {
+        lock (_locker)
+        {
+            singleThreadLoggerTarget.Publish(context, renderer, cancellationToken);
+        }
+    }
+
+    public override void Dispose(CancellationToken cancellationToken)
+    {
+        lock (_locker)
+        {
+            singleThreadLoggerTarget.Dispose(cancellationToken);
+        }
+    }
+}
