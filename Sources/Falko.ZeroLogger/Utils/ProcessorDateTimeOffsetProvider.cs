f1b7cf44adca1ec73b3abb9b112c489d851a6f96
Rima Falko <rima@falko.email>
2025-09-29 16:23:53 +0200

    optimize processor date-time-offset provider for improved performance and do that more thread safety

--- Sources/Falko.ZeroLogger/Utils/ProcessorDateTimeOffsetProvider.cs
+++ Sources/Falko.ZeroLogger/Utils/ProcessorDateTimeOffsetProvider.cs
@@ -1,40 +1,111 @@
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace System.Logging.Utils;
 
 internal sealed class ProcessorDateTimeOffsetProvider : IDateTimeOffsetProvider
 {
-    private const long CachedTimeUpdateIntervalTicks = 10 * 1000 * 1000; // 10ms
+    private const long CachedTimeUpdateThresholdMilliseconds = 12;
 
-    private readonly Stopwatch _stopwatch = Stopwatch.StartNew();
+    private readonly Stopwatch _stopwatch;
 
-    private readonly float _stopwatchTickToDateTimeTicks = TimeSpan.TicksPerSecond / (float)Stopwatch.Frequency;
+    private readonly long _stopwatchTicksThreshold;
 
-    private DateTimeOffset _cachedTime = DateTimeOffset.Now;
+    private readonly double _stopwatchTicksRatio;
 
-    private long _lastTicksTime;
+    private DateTimeOffsetSnapshot _snapshot;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ProcessorDateTimeOffsetProvider() => _lastTicksTime = _stopwatch.ElapsedTicks;
+    public ProcessorDateTimeOffsetProvider()
+    {
+        _stopwatch = Stopwatch.StartNew();
+
+        var stopwatchTicksThreshold = Stopwatch.Frequency * CachedTimeUpdateThresholdMilliseconds / 1000;
+        _stopwatchTicksThreshold = stopwatchTicksThreshold <= 0 ? 1 : stopwatchTicksThreshold;
+
+        _stopwatchTicksRatio = (double)TimeSpan.TicksPerSecond / Stopwatch.Frequency;
+
+        var rootUtcTime = DateTime.UtcNow;
+        var rootOffsetTime = TimeZoneInfo.Local.GetUtcOffset(rootUtcTime);
+
+        _snapshot = new DateTimeOffsetSnapshot
+        {
+            SnapshotVersion = 0,
+            LocalTicks = rootUtcTime.Ticks + rootOffsetTime.Ticks,
+            LocalOffset = rootOffsetTime,
+            StopwatchTicks = _stopwatch.ElapsedTicks
+        };
+    }
 
     public DateTimeOffset Now
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
         {
-            var currentTicks = _stopwatch.ElapsedTicks;
+            RetryRead:
 
-            var currentTicksDelta = currentTicks - _lastTicksTime;
+            var snapshotVersionToRead = Volatile.Read(ref _snapshot.SnapshotVersion);
 
-            if (currentTicksDelta < CachedTimeUpdateIntervalTicks)
+            if ((snapshotVersionToRead & 1) != 0)
             {
-                return _cachedTime.AddTicks((long)(currentTicksDelta * _stopwatchTickToDateTimeTicks));
+                goto RetryRead;
             }
 
-            _cachedTime = DateTimeOffset.Now;
-            _lastTicksTime = currentTicks;
+            var currentSnapshot = _snapshot;
 
-            return _cachedTime;
+            var currentLocalTicks = currentSnapshot.LocalTicks;
+            var currentLocalOffset = currentSnapshot.LocalOffset;
+            var currentStopwatchTicks = currentSnapshot.StopwatchTicks;
+
+            var snapshotVersionToWrite = Volatile.Read(ref _snapshot.SnapshotVersion);
+
+            if (snapshotVersionToRead != snapshotVersionToWrite
+                || (snapshotVersionToWrite & 1) != 0)
+            {
+                goto RetryRead;
+            }
+
+            var deltaStopwatchTicks = _stopwatch.ElapsedTicks - currentStopwatchTicks;
+
+            if (deltaStopwatchTicks < _stopwatchTicksThreshold)
+            {
+                var addDeltaTicks = (long)(deltaStopwatchTicks * _stopwatchTicksRatio);
+                return new DateTimeOffset(currentLocalTicks + addDeltaTicks, currentLocalOffset);
+            }
+
+            if (Interlocked.CompareExchange(ref _snapshot.SnapshotVersion,
+                    snapshotVersionToWrite + 1,
+                    snapshotVersionToWrite)
+                != snapshotVersionToWrite)
+            {
+                goto RetryRead;
+            }
+
+            var nextUtcTime = DateTime.UtcNow;
+            var nextOffsetTime = TimeZoneInfo.Local.GetUtcOffset(nextUtcTime);
+
+            var nextLocalTicks = nextUtcTime.Ticks + nextOffsetTime.Ticks;
+            var nextStopwatchTicks = _stopwatch.ElapsedTicks;
+
+            _snapshot.LocalTicks = nextLocalTicks;
+            _snapshot.LocalOffset = nextOffsetTime;
+            _snapshot.StopwatchTicks = nextStopwatchTicks;
+
+            Volatile.Write(ref _snapshot.SnapshotVersion, snapshotVersionToWrite + 2);
+
+            return new DateTimeOffset(nextLocalTicks, nextOffsetTime);
         }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct DateTimeOffsetSnapshot
+    {
+        public int SnapshotVersion;
+
+        public long LocalTicks;
+
+        public TimeSpan LocalOffset;
+
+        public long StopwatchTicks;
     }
 }
--- Sources/Falko.ZeroLogger/Utils/SystemDateTimeOffsetProvider.cs
+++ Sources/Falko.ZeroLogger/Utils/SystemDateTimeOffsetProvider.cs
@@ -1,7 +1,6 @@
 namespace System.Logging.Utils;
 
-[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
-internal sealed class SystemDateTimeOffsetProvider() : IDateTimeOffsetProvider
+public sealed class SystemDateTimeOffsetProvider : IDateTimeOffsetProvider
 {
     public DateTimeOffset Now
     {
