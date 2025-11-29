b7f5f198a523b712cb9115cd47d364add38d73e9
Rima Falko <rima@falko.email>
2025-11-29 01:45:15 +0100

    #25 - optimize message rendering by removing volatile keyword for presistent log-message renderer

--- Sources/Falko.Logging/Renderers/PersistentLogMessageRenderer.cs
+++ Sources/Falko.Logging/Renderers/PersistentLogMessageRenderer.cs
@@ -2,16 +2,19 @@ namespace Falko.Logging.Renderers;
 
 internal abstract class PersistentLogMessageRenderer : ILogMessageRenderer
 {
-    private volatile string? _message;
+    private string? _message;
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public string Render()
     {
-        if (_message is not null) return _message;
+        var currentMessage = _message;
+        if (currentMessage is not null) return currentMessage;
 
-        var message = RenderCore();
-        _message = message;
-        return message;
+        var newMessage = RenderCore();
+        _message = newMessage;
+        return newMessage;
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     protected abstract string RenderCore();
 }
